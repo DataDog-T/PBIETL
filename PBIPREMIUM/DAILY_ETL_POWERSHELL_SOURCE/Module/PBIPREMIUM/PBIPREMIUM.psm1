e0aebeb0f81fb92bb64daf8107a58c247bc877c9
DataDog-T <victoriamccunn@gmail.com>
2020-01-21 13:17:32 -0600

    Update to get PBIPremiumCubeDmvs

--- PBIPREMIUM/DAILY_ETL_POWERSHELL_SOURCE/Module/PBIPREMIUM/PBIPREMIUM.psm1
+++ PBIPREMIUM/DAILY_ETL_POWERSHELL_SOURCE/Module/PBIPREMIUM/PBIPREMIUM.psm1
@@ -1,19 +1,44 @@
-﻿$global:folderFullPath = 'C:\Users\test\PBIPREMIUM\'
-$env:ACCOUNT_NAME = "test"
+﻿$global:folderFullPath = 'C:\Users\Test\PBIPREMIUM\'
+$env:ACCOUNT_NAME = "Test"
 $env:AZCOPY_LOG_LOCATION = "C:\Program Files\WindowsPowerShell\Logs\AZCOPYLOGS"
 $env:AZCOPY_JOB_PLAN_LOCATION = "C:\Program Files\WindowsPowerShell\Logs\AZCOPYPLANS"
 $global:previousDay_yyyMMdd = (get-date).AddDays(-1).ToString("yyyMMdd") | foreach {$_ -replace ":", "."}
 $global:currentDay_yyyMMdd = (get-date).ToString("yyyMMdd_HH:mm:ss") | foreach {$_ -replace ":", "."}
 $global:Logfilepath = $null
 $global:pbiAdminCredFile= "C:\Users\test\powerbicredssecure.txt"
-$global:pbiAdminUserName = "test@test.com"
+$global:pbiAdminUserName = "test&test.com"
 $global:datalakePath = "https://test.dfs.core.windows.net/staging/PBIPREMIUM/"
 $global:logDatalakePath = "https://test.dfs.core.windows.net/staging/Logs/PBIPREMIUM/"
 $global:AZCOPYMessageContent = $null
 $global:sourceTable = $null
+$global:datasetName = $null
 $global:AZCOPYMessageType = (C:\Windows\azcopy\azcopy.exe jobs list --output-type json | ConvertFrom-Json | Select-Object MessageType -OutVariable string -First 1)
 $global:credential = New-Object -TypeName System.Management.Automation.PSCredential `
  -ArgumentList $pbiAdminUserName, (Get-Content $pbiAdminCredFile | ConvertTo-SecureString) 
+$global:attributeHierarchyStoragesSQL = 'Select * from $SYSTEM.TMSCHEMA_ATTRIBUTE_HIERARCHY_STORAGES' #distinct data count for each column
+$global:attributeHierarchies = 'Select * from $SYSTEM.TMSCHEMA_ATTRIBUTE_HIERARCHIES'  #ties hierarchy id to column
+$global:columnStoragesSQL = 'SELECT * from $SYSTEM.TMSCHEMA_COLUMN_STORAGES' #has order by column, row count is inaccurate
+$global:columnsSQL = 'Select * from $SYSTEM.TMSCHEMA_COLUMNS' #column name, ID for table, data type, category, hidden, iskey, isunique, is nullable, summarize by, expression for calc columns, hierarchy id, refresh time, modify time. source provider type, display folder
+$global:dataSourcesSQL = 'SELECT * from $SYSTEM.TMSCHEMA_DATA_SOURCES' #connection string, account, impersonation mode, name
+$global:hierarchiesSQL = 'Select * from $SYSTEM.TMSCHEMA_HIERARCHIES' #hierarchy name, display folder
+$global:hierarchyStoragesSQL = 'Select * from $SYSTEM.TMSCHEMA_HIERARCHY_STORAGES' #user hierarchy definitions
+$global:KpiSQL = 'Select * from $SYSTEM.TMSCHEMA_KPIS' #KPI definition
+$global:levelsSQL = 'Select * from $SYSTEM.TMSCHEMA_LEVELS' #hierarchy level and model source column
+$global:measuresSQL = 'Select * from $SYSTEM.TMSCHEMA_MEASURES' #measure and expressions, formatt, hidden, display folder
+$global:modelSQL = 'Select * from $SYSTEM.TMSCHEMA_MODEL' #name of each model
+$global:partitionsSQL = 'Select * from $SYSTEM.TMSCHEMA_PARTITIONS' #source queries for each table
+$global:perspectiveColumnsSQL = 'Select * from $SYSTEM.TMSCHEMA_PERSPECTIVE_COLUMNS' #perspective table to perspective column maps
+$global:perspectiveHierarchiesSQL = 'Select * from $SYSTEM.TMSCHEMA_PERSPECTIVE_HIERARCHIES' # perspective table to hierarchy id map
+$global:perspectiveMeasuresSQL = 'Select * from $SYSTEM.TMSCHEMA_PERSPECTIVE_MEASURES' # perspective table id to measure id map
+$global:perspectiveTablesSQL = 'Select * from $SYSTEM.TMSCHEMA_PERSPECTIVE_TABLES' # perspective to table map
+$global:perspectivesSQL = 'Select * from $SYSTEM.TMSCHEMA_PERSPECTIVES' # list of perspectives
+$global:relationshipsSQL = 'Select * from $SYSTEM.TMSCHEMA_RELATIONSHIPS' # active , type, crossfilter, table, colmn, cardinality
+$global:rolemembershipsSQL = 'Select * from $SYSTEM.TMSCHEMA_ROLE_MEMBERSHIPS' # member name, ID, Modified time, role id,
+$global:rolesSQL = 'Select * from $SYSTEM.TMSCHEMA_ROLES' # model, name, description, permission
+$global:tablepermissionsSQL = 'Select * from $SYSTEM.TMSCHEMA_TABLE_PERMISSIONS' # role, rable, filter expression, modified time
+$global:tablesSQL = 'Select * from $SYSTEM.TMSCHEMA_TABLES'  #tables, description, hidden, 
+$global:catalogSQL = 'Select * from $SYSTEM.DBSCHEMA_CATALOGS' #Catalog name, description, compatibility level, type, database id, version
+$global:schemasSQL = 'SELECT * FROM $System.DBSchema_Tables'
  
 
 function azCopyComputeRunningJobID ($fileToCopy,$datalakeFullPath)
@@ -35,6 +60,13 @@ function ComputeNewValueazcopy ($jobsid)
 function ComputeNewDataCheckQuery ($sourceTable)
 {
    "EVALUATE SUMMARIZE(Filter(" + $sourceTable + "," + "DATEDIFF(" + $sourceTable + "[timestamp]" + "," + "TODAY()" +"," + "DAY" + ")" + "=1" + ")" + "," + '"Hours"' +"," + "DISTINCTCOUNT(" + $sourceTable + "[timestamp]" + "))"
+}
+
+function ComputeNewDatasetName ($datasetName)
+{
+[Parameter(Mandatory)]
+[string]$datasetName
+
 }
 
 
@@ -236,3 +268,74 @@ function Get-CurrentLineNumber {
 function Get-CurrentFileName {
     $MyInvocation.ScriptName
 }
+
+
+function PbiDataSchemaExport{
+    param(
+          [string] $ServerName = 'https://analysis.windows.net/powerbi/api',
+          [string] $DBName   = $datasetId,
+          [string] $sqlCommand,
+          [string] $outFile,
+          [string] $auth
+    )
+
+ Try { Write-Log -Message 'Connect-PowerBIServiceAccount -Credential $credential' -Type "CommandStart"
+    Connect-PowerBIServiceAccount -Credential $credential
+ }
+    catch
+    { Write-Log -Message $_.Exception.Message -Type ($_.Exception.GetType().FullName) }
+    Write-Log -Message 'Connect-PowerBIServiceAccount -Credential $credential' -Type "CommandEnd"
+
+    $auth = (Get-PowerBIAccessToken).Authorization
+    $UserName = $credential.UserName
+    $PlainPassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($credential.Password))
+    Try { Write-Log -Message 'Invoke-PowerBIRestMethod -Method Get -Url "https://api.powerbi.com/v1.0/myorg/datasets" | ConvertFrom-Json' -Type "CommandStart"
+    $datasets =
+     Invoke-PowerBIRestMethod -Method Get -Url "https://api.powerbi.com/v1.0/myorg/datasets" | ConvertFrom-Json 
+     }
+     catch
+{
+	Write-Log -Message $_.Exception.Message -Type ($_.Exception.GetType().FullName)
+}
+Write-Log -Message 'Invoke-PowerBIRestMethod -Method Get -Url "https://api.powerbi.com/v1.0/myorg/datasets" | ConvertFrom-Json' -Type "CommandEnd"
+
+
+    $premiumAppDataset = $datasets |        
+        foreach-Object { $_.value } |
+        Where-Object { $_.name -eq "$datasetName"} |
+        Select-Object -First 1
+
+    if($premiumAppDataset){
+ Try {
+    Write-Log -Message 'Creating Premium Capacity Metrics App OLEDB MSOLAP Connection to Extract Information' -Type "CommandStart"
+        $region = ([System.Uri]$datasets.'@odata.context').Host
+        $datasetId = $premiumAppDataset.id
+        $cs = "Provider=MSOLAP;Data Source=https://analysis.windows.net/powerbi/api;;Initial Catalog=$datasetId;Location=https://$region/xmla?vs=sobe_wowvirtualserver&db=$datasetId;MDX Compatibility= 1; MDX Missing Member Mode= Error; Safety Options= 2; Update Isolation Level= 2; Locale Identifier= 1033;User Id = $UserName; Password=$PlainPassword"
+        $connection = New-Object System.Data.OleDb.OleDbConnection $cs
+        $connection.Open()
+        $command = New-Object System.Data.OleDb.OleDbCommand -ArgumentList $sqlCommand, $connection
+        $rdr = $command.ExecuteReader()
+        $result = New-Object System.Collections.ArrayList
+        while($rdr.Read()){
+            $properties = @{}
+            for($fieldIndex = 0; $fieldIndex -lt $rdr.FieldCount; $fieldIndex ++){
+                $properties[$rdr.GetName($fieldIndex)] = $rdr[$fieldIndex]
+            }
+            $row = New-Object PSObject -Property $properties
+            $void = $result.Add($row)
+        }
+        $rdr.Close()
+        $connection.Close()        
+        $result | Select-Object * | Export-Csv -Path $outFile -NoTypeInformation -Force
+    }
+        catch
+        {
+        Write-Log -Message $_.Exception.Message -Type ($_.Exception.GetType().FullName)
+        }
+       Write-Log -Message 'Creating Premium Capacity Metrics App OLEDB MSOLAP Connection to Extract Information' -Type "CommandEnd" 
+    }
+    else{
+        Write-Log -Message "Premium Capacity Metrics app not found. Please install the app and try again."   -Type "Exception-AppNotFoundorInstalled"     
+    }
+    }
+   
