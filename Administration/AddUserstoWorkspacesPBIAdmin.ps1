c74c46b3d0c58c7098d3152adc287b5d53e25ea0
DataDog-T <victoriamccunn@gmail.com>
2020-02-21 12:28:16 -0600

    New Admin Script to Add Users to Workspaces in Bulk

---
+++ Administration/AddUserstoWorkspacesPBIAdmin.ps1
@@ -0,0 +1,17 @@
+﻿$credential = (Get-Credential)
+Connect-PowerBIServiceAccount -Credential $credential
+
+$workspaces = "Test",
+"Test 2"
+
+$users = 
+"test@test.com",
+"test2@test.com"
+
+
+$ids = Foreach($name in $workspaces) {Get-PowerBIWorkspace -Scope Organization -Filter "name eq '$name'" }
+
+Foreach ($user in $users) {
+Foreach($id in $ids) {
+Add-PowerBIWorkspaceUser -Scope Organization -Id $id.id -UserEmailAddress $user -AccessRight Admin
+}}
